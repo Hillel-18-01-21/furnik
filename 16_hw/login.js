444ab12062552e5a5a874939013c92fd57891d56
Serhii Furnik <furnik267@gmail.com>
2021-04-08 18:10:36 +0300

    hw16

---
+++ 16_hw/login.js
@@ -0,0 +1,49 @@
+
+class LogIn {
+    constructor(container) {
+        this.containerEl = container;
+        this.loginContainerEl = this.containerEl.querySelector('#login');
+        this.url = "https://reqres.in/api/login";
+    }
+
+
+    resetDefault(evt) {
+        evt.preventDefault();
+    }
+
+    handleLogin = () => {
+        this.loginEl = this.loginContainerEl.querySelector('#loginInput');
+        this.passwordEl = this.loginContainerEl.querySelector('#passwordInput');
+        if(this.loginEl.value == "" || this.passwordEl.value == "") {
+                return;        
+        };
+
+        this.promise = fetch(this.url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                "email": this.loginEl.value,
+                "password": this.passwordEl.value
+            }),
+        });
+
+        this.promise.then(response => {
+            if(!response.ok) {
+                document.querySelector('#error').classList.remove('d-none');
+                this.passwordEl.value = null;
+            } else {
+                this.loginContainerEl.classList.add('d-none');
+                this.containerEl.querySelector('#list-container').classList.remove('d-none');
+            }; 
+        });   
+    };
+
+    init() {
+        this.formEl = this.containerEl.querySelector('form');
+
+        this.formEl.addEventListener('submit', this.handleLogin);
+        this.formEl.addEventListener('submit', this.resetDefault);
+    };
+}
